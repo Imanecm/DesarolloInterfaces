5c1df01a2d02eeda08884a6dd483631c273f4d5f
mouddenbou <mouddenbou@gmail.com>
2025-10-29 10:40:02 +0100

    CategoriasHechas

--- Pagina web/js/main-menu.js
+++ Pagina web/js/main-menu.js
@@ -2,21 +2,49 @@ const rootDirectory = window.location.origin;
 
 function createMenuSection(section) {
     const menuSection = document.createElement('li');
-
-    menuSection.innerHTML = `
-        <a href="${section.reference}" class="menu-links">${section.name}</a>
-    `;
-
+    
+    if (section.hasDropdown) {
+        menuSection.classList.add('dropdown');
+        menuSection.innerHTML = `
+            <a href="${section.reference}" class="menu-links dropdown-toggle">${section.name}</a>
+            <div class="dropdown-menu categories-dropdown">
+                <div class="dropdown-content">
+                    <div class="categories-column">
+                        <h4>Películas y Anime</h4>
+                        <ul>
+                            <li><a href="categoria.html?tipo=peliculas">Películas</a></li>
+                            <li><a href="categoria.html?tipo=anime">Anime</a></li>
+                            <li><a href="categoria.html?tipo=series">Series</a></li>
+                        </ul>
+                    </div>
+                    <div class="categories-column">
+                        <h4>Géneros</h4>
+                        <ul>
+                            <li><a href="categoria.html?genero=Drama">Drama</a></li>
+                            <li><a href="categoria.html?genero=Comedia">Comedia</a></li>
+                            <li><a href="categoria.html?genero=Acción">Acción</a></li>
+                            <li><a href="categoria.html?genero=Aventura">Aventura</a></li>
+                            <li><a href="categoria.html?genero=Ciencia Ficción">Ciencia Ficción</a></li>
+                            <li><a href="categoria.html?genero=Terror">Terror</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        `;
+    } else {
+        menuSection.innerHTML = `<a href="${section.reference}" class="menu-links">${section.name}</a>`;
+    }
+    
     document.getElementById('menu-main-list').appendChild(menuSection);
 }
 
+// Cargar el menú
 fetch('data/data.json')
-.then(response => response.json())
-.then(data => {
-	const sections = data.mainmenu_items;
-
-	sections.forEach(createMenuSection);
-})
-.catch(error => {
-	console.error('Error al cargar el archivo JSON:', error);
-});
+    .then(response => response.json())
+    .then(data => {
+        const sections = data.mainmenu_items;
+        sections.forEach(createMenuSection);
+    })
+    .catch(error => {
+        console.error('Error al cargar el archivo JSON:', error);
+    });
